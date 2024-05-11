d4be6091784d871b6cdbae6158face522d4a542c
Ledmington <filippo.barbari@gmail.com>
2024-05-11 17:12:38 +0200

    Removed picocli

--- src/main/java/com/ledmington/spiq/Spiq.java
+++ src/main/java/com/ledmington/spiq/Spiq.java
@@ -8,11 +8,43 @@
  */
 package com.ledmington.spiq;
 
-import picocli.CommandLine;
+import java.io.File;
+import java.io.IOException;
+
+import org.antlr.v4.runtime.CharStreams;
+
+import com.ledmington.spiq.interpreter.SpiqInterperter;
+import com.ledmington.spiq.utils.SpiqUtils;
 
 public final class Spiq {
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new SpiqCli()).execute(args);
-        System.exit(exitCode);
+
+    public static void main(final String[] args) {
+        System.err.println(SpiqUtils.nlJoin(
+                "",
+                " ________  ________  ___  ________      ",
+                "|\\  _____\\|\\   __  \\|\\  \\|\\   __  \\     ",
+                "\\ \\  \\____| \\  \\_\\  \\ \\  \\ \\  \\ \\  \\    ",
+                " \\ \\_____  \\ \\   ____\\ \\  \\ \\  \\ \\  \\   ",
+                "  \\|____|\\  \\ \\  \\___|\\ \\  \\ \\  \\_\\  \\  ",
+                "     ___\\_\\  \\ \\__\\    \\ \\__\\ \\_____  \\ ",
+                "    |\\________\\|__|     \\|__|\\|___| \\__\\",
+                "    \\|_________|                   \\|__|\n"));
+
+        final String filename;
+        if (args.length == 0) {
+            System.err.println("Expected a filename.");
+            System.exit(-1);
+        }
+
+        filename = args[0];
+
+        System.out.println("Reading \"" + filename + "\"");
+
+        final SpiqInterperter compiler = new SpiqInterperter();
+        try {
+            compiler.compile(CharStreams.fromFileName(new File(filename).getAbsolutePath()));
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
