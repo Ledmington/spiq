fe1da57acacf4c29b6def279abd1b38ba9a39fea
Ledmington <ledmington.dev@gmail.com>
2024-05-14 00:23:33 +0200

    Changed license to GNU GPLv3

--- lexer/src/main/java/com/ledmington/spiq/NumberLiteral.java
+++ lexer/src/main/java/com/ledmington/spiq/NumberLiteral.java
@@ -1,11 +1,20 @@
 /*
- * Copyright (C) 2022-2024 Filippo Barbari <filippo.barbari@gmail.com>
- *
- * This file is part of spiq.
- *
- * spiq can not be copied and/or distributed without
- * the express permission of Filippo Barbari.
- */
+* spiq - Human readable programming language
+* Copyright (C) 2022-2024 Filippo Barbari <filippo.barbari@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.ledmington.spiq;
 
 import java.math.BigDecimal;
